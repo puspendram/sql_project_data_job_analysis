4a9dda6d5fa2e6f493d30d85e38be52662f2d57c
puspendramsathua <pintossoou@gmail.com>
2025-03-22 20:28:03 +0530

    2

---
+++ Project_Sql/4_Top_skills_for_best_pay.sql
@@ -0,0 +1,14 @@
+SELECT 
+skills,
+round(avg (salary_year_avg),0) as avg_salary
+FROM job_postings_fact  
+INNER JOIN skills_job_dim ON job_postings_fact.job_id=skills_job_dim.job_id
+INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+WHERE 
+job_title_short = 'Data Analyst' AND
+salary_year_avg is not null 
+GROUP BY 
+skills 
+order BY
+avg_salary DESC
+LIMIT 25 
